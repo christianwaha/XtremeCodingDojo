ccc024ea3266f07971f857a81b473131e183618b
cwlabs <christian.waha@outlook.com>
2015-10-24 17:11:08 +0200

    TrafficLight Implementation
    Team Ampelsteuerung 1

---
+++ Windows 10 IoT Dojo Munich/Team AmpelSteuerung/Trafficlight2/TrafficApp/Light.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+
+namespace TrafficApp
+{
+    public enum Color { Red = 12, Yellow = 26, Green = 21 }
+    public class Light
+    {
+        GpioController controller = GpioController.GetDefault();
+        GpioPin gpiPin;
+        Color color;
+        public static List<object> Sequence { get; set; }
+        public static Dictionary<Color,Light> Leds { get; set; }
+        public static void InitLights()
+        {
+            Sequence = new List<object>();
+            Leds = new Dictionary<Color, Light>();
+            foreach (Color c in Enum.GetValues(typeof(Color)))
+            {
+                Leds.Add(c, new Light(c));
+            }
+        }
+        public Light(Color color)
+        {
+
+            if (controller == null)
+            {
+                return;
+            }
+
+            if (this.gpiPin == null)
+            {
+                this.color = color;
+                this.gpiPin = controller.OpenPin((int)color, GpioSharingMode.Exclusive);
+                this.gpiPin.SetDriveMode(GpioPinDriveMode.Output);
+            }
+
+        }
+
+        //public void Close()
+        //{
+        //    gpiPin.Dispose();
+        //}
+
+        public bool IsOn
+        {
+            get
+            {
+                return this.gpiPin.Read() == GpioPinValue.High;
+            }
+        }
+        public void SwitchOn()
+        {
+            gpiPin.Write(GpioPinValue.High);
+            Sequence.Add(new Tuple<Color, string>(this.color, gpiPin.Read().ToString()));
+        }
+
+        internal void SwitchOff()
+        {
+            gpiPin.Write(GpioPinValue.Low);
+            Sequence.Add(new Tuple<Color, string>(this.color, gpiPin.Read().ToString()));
+        }
+
+        internal void RunSequence(bool clearlist = true)
+        {
+            if (clearlist)
+            {
+                Sequence.Clear();
+            }
+            this.SwitchOn();
+            Task.Delay(3000).Wait();
+            Sequence.Add(new Tuple<Color, string>(this.color, "3000ms"));
+            this.SwitchOff();
+        }
+    }
+}
---
+++ Windows 10 IoT Dojo Munich/Team AmpelSteuerung/Trafficlight2/TrafficApp/Test.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficApp
+{
+    public class Test
+    {
+        static public bool assert(bool condition)
+        {
+            if (!condition)
+            {
+                throw new NotSupportedException("Assertion failed!");
+            }
+            else
+                return true;
+        }
+
+        static public void RunTests()
+        {
+            Light.InitLights();
+            assert(Test.TestIfRedLEDisOn());
+            assert(Test.TestIfRedLEDisOff());
+
+            assert(Test.TestIfYellowLEDisOn());
+            assert(Test.TestIfYellowLEDisOff());
+
+            assert(Test.TestIfGreenLEDisOn());
+            assert(Test.TestIfGreenLEDisOff());
+
+            assert(Test.TestRedSequence());
+            assert(Test.TestYellowSequence());
+            assert(Test.TestGreenSequence());
+
+            assert(Test.TestCompleteSequence());
+
+            assert(Test.TestIfAllLEDOn());
+        }
+
+        static public bool TestCompleteSequence()
+        {
+            var ledRed = Light.Leds[Color.Red];
+            ledRed.RunSequence();
+            var ledYellow = Light.Leds[Color.Yellow];
+            ledYellow.RunSequence(false);
+            var ledGreen = Light.Leds[Color.Green];
+            ledGreen.RunSequence(false);
+            ledYellow.RunSequence(false);
+            ledRed.RunSequence(false);
+            var expectedSequence = new List<object>();
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "High"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "3000ms"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "Low"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "High"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "3000ms"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "Low"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Green, "High"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Green, "3000ms"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Green, "Low"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "High"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "3000ms"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "Low"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "High"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "3000ms"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "Low"));
+
+            return (expectedSequence.SequenceEqual(Light.Sequence));
+        }
+
+        static internal bool TestIfRedLEDisOn()
+        {
+            var redled = Light.Leds[Color.Red];
+            redled.SwitchOn();
+            var value = redled.IsOn;
+
+            return value;
+        }
+
+        static internal bool TestIfRedLEDisOff()
+        {
+            var redled = Light.Leds[Color.Red];
+            redled.SwitchOff();
+            var value = redled.IsOn;
+            return !value;
+        }
+
+        static internal bool TestIfYellowLEDisOn()
+        {
+            var yellowled = Light.Leds[Color.Yellow];
+            yellowled.SwitchOn();
+            var value = yellowled.IsOn;
+            return value;
+        }
+
+        static internal bool TestIfYellowLEDisOff()
+        {
+            var yellowled = Light.Leds[Color.Yellow];
+            yellowled.SwitchOff();
+            var value = yellowled.IsOn;
+            return !value;
+        }
+
+        static internal bool TestIfGreenLEDisOn()
+        {
+            var Greenled = Light.Leds[Color.Green];
+            Greenled.SwitchOn();
+            var value = Greenled.IsOn;
+            return value;
+        }
+
+        static internal bool TestIfGreenLEDisOff()
+        {
+            var Greenled = Light.Leds[Color.Green];
+            Greenled.SwitchOff();
+            var value = Greenled.IsOn;
+            return !value;
+        }
+
+        static internal bool TestIfAllLEDOn()
+        {
+            var value = true;
+            foreach (Color c in Enum.GetValues(typeof(Color)))
+            {
+                var led = Light.Leds[c];
+                led.SwitchOn();
+                value &= led.IsOn;
+            }
+            //foreach (Color c in Enum.GetValues(typeof(Color)))
+            //{
+            //    var led = new Light(c);
+            //    value &= led.IsOn;
+            //    led.Close();
+            //}
+            return value;
+        }
+
+        static internal bool TestRedSequence()
+        {
+            var redled = Light.Leds[Color.Red];
+            redled.RunSequence();
+            var expectedSequence = new List<object>();
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "High"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "3000ms"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Red, "Low"));
+
+            return (expectedSequence.SequenceEqual(Light.Sequence));
+        }
+
+        static internal bool TestYellowSequence()
+        {
+            var led = Light.Leds[Color.Yellow];
+            led.RunSequence();
+            var expectedSequence = new List<object>();
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "High"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "3000ms"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Yellow, "Low"));
+
+            return (expectedSequence.SequenceEqual(Light.Sequence));
+        }
+
+        static internal bool TestGreenSequence()
+        {
+            var led = Light.Leds[Color.Green];
+            led.RunSequence();
+            var expectedSequence = new List<object>();
+            expectedSequence.Add(new Tuple<Color, string>(Color.Green, "High"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Green, "3000ms"));
+            expectedSequence.Add(new Tuple<Color, string>(Color.Green, "Low"));
+
+            return (expectedSequence.SequenceEqual(Light.Sequence));
+        }
+    }
+}
